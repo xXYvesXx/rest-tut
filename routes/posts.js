f352fc0e24b353cf0f4944a34fb6b1d2326711f3
xXYvesXx <yvesh20@gmail.com>
2020-12-03 15:53:49 -0300

    refatoração do código

--- routes/posts.js
+++ routes/posts.js
@@ -2,12 +2,28 @@ const express = require('express')
 const router = express.Router()
 const Post = require('../models/Post')
 //Post route
-router.get('/',(req,res)=>{
-    res.send('We are on posts')
+router.get('/', async (req,res)=>{
+    try {
+        const posts = await Post.find()
+        res.json(posts)
+    } catch (err) {
+        res.json({message:err})
+    }
+   
 })
 
-router.post('/',(req,res)=>{
-    console.log(req.body)
+router.post('/', async (req,res)=>{
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    })
+    try{
+        const savedPost = await post.save()
+         res.json(savedPost)
+    }catch(err){
+        res.json({ message: err})
+    }
+    
 })
 
 module.exports = router
