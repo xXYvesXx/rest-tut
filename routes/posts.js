0285aae06de29bd872a8f87b1ca09b9cfbc7d152
xXYvesXx <yvesh20@gmail.com>
2020-12-03 16:45:57 -0300

    implementação dos métodos de encontrar,deletar e atualizar um post específico

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const Post = require('../models/Post')
-//Post route
+
+
+//Get all posts
 router.get('/', async (req,res)=>{
     try {
         const posts = await Post.find()
@@ -12,6 +14,7 @@ router.get('/', async (req,res)=>{
    
 })
 
+//Submit a post
 router.post('/', async (req,res)=>{
     const post = new Post({
         title: req.body.title,
@@ -26,4 +29,38 @@ router.post('/', async (req,res)=>{
     
 })
 
+//Specific post
+router.get('/:postId', async (req,res) =>{
+    try {
+        const post = await Post.findById(req.params.postId)
+    res.json(post)
+    } catch (err) {
+        res.json({message: err})
+    }
+    
+})
+
+//Delete Post
+router.delete('/:postId',async (req,res)=>{
+    try {
+        const removedPost = await Post.remove({_id: req.params.postId})
+            res.json(removedPost)
+    } catch (err) {
+        res.json({message: err})
+    }
+    
+})
+
+//Update post
+router.patch('/:postId',async (req,res)=>{
+    try {
+        const updatedPost = await Post.updateOne(
+            {_id: req.params.postId},
+            { $set: { title: req.body.title} }
+            )
+        res.json(updatedPost)
+    } catch (err) {
+        res.json({ message:err })
+    }
+})
 module.exports = router
